40ebf94c40962ffa5186a06d93bf1fe79068b4e2
vagg979 <53450675+vagg979@users.noreply.github.com>
2020-10-06 08:50:39 -0500

    Update views.py

--- Modulos/Academica/views.py
+++ Modulos/Academica/views.py
@@ -12,7 +12,7 @@ def contactar(request):
         asunto = request.POST["txtAsunto"]
         mensaje = request.POST["txtMensaje"] + " / Email: " + request.POST["txtEmail"]
         email_desde = settings.EMAIL_HOST_USER
-        email_para = ["vagg979@gmail.com"]
+        email_para = ["mail_destino"]
         send_mail(asunto, mensaje, email_desde, email_para, fail_silently=False)
         return render(request, "contactoexitoso.html")
     return render(request, "formularioContacto.html")
