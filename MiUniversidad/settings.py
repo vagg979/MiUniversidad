b72ef96536aafdceeea6ed064e39851dc1753bbf
vagg979 <53450675+vagg979@users.noreply.github.com>
2020-10-06 08:57:18 -0500

    Update settings.py

--- MiUniversidad/settings.py
+++ MiUniversidad/settings.py
@@ -124,5 +124,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "sanpoc10@gmail.com"
-EMAIL_HOST_PASSWORD = "sangie1026"
+EMAIL_HOST_USER = "correo_gmail"
+EMAIL_HOST_PASSWORD = "contraseña_correo"
